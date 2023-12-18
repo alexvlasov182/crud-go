c7b24cb3cc929440e76278cebdd83b2b3b9c076d
Oleksandr Vlasov <drumlife182@gmail.com>
2023-12-18 19:57:59 +0100

    Added handlers in the crud application

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+
+	crudgo "github.com/alexvlasov182/crud-go"
+	"github.com/alexvlasov182/crud-go/pkg/handler"
+)
+
 func main() {
-	println("Golnag server crud application")
+	handlers := new(handler.Handler)
+	srv := new(crudgo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running server %s", err.Error())
+	}
 }
---
+++ user.go
@@ -0,0 +1,8 @@
+package crudgo
+
+type User struct {
+	Id       int    `json:"-"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
