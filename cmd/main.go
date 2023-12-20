7f5024c16b84e59d7d517b9a1dc702f827cfde69
Oleksandr Vlasov <drumlife182@gmail.com>
2023-12-20 12:40:29 +0100

    Worked with clean architecture

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,16 @@ import (
 
 	crudgo "github.com/alexvlasov182/crud-go"
 	"github.com/alexvlasov182/crud-go/pkg/handler"
+	"github.com/alexvlasov182/crud-go/pkg/repository"
+	"github.com/alexvlasov182/crud-go/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(crudgo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running server %s", err.Error())
